c7aff46e69e49d2d7e4724eaa9cbba86d8cd452f
Henry-Cho <hyungseok5926@gmail.com>
2021-04-10 17:40:51 +0900

    Henry commit

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
 
 function Test() {
-  return <div>test</div>;
+  return (
+    <div>
+      test
+      {/* <Picker onSelect={this.addEmoji} /> */}
+      <Picker
+        title="Pick your emoji…"
+        emoji="point_up"
+        enableFrequentEmojiSort={true}
+        perLine={5}
+        set={"facebook"}
+      />
+      {/* <Picker style={{ position: "absolute", bottom: "20px", right: "20px" }} /> */}
+    </div>
+  );
 }
 
 export default Test;
