f1e355a2855b6bbe3a64bf7dd6154ac82070e917
Henry-Cho <hyungseok5926@gmail.com>
2021-04-13 00:31:23 +0900

    add post component

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import Post from "./Post";
 
 const PostList = (props) => {
   return (
     <PostListFrame>
-      <div></div>
+      <Post />
+      <Post />
     </PostListFrame>
   );
 };
@@ -15,4 +17,20 @@ const PostListFrame = styled.div`
   width: 100%;
   min-height: 80vh;
   background: #ffffff;
+  padding: 8px 12px;
+  height: 80vh;
+  overflow: auto;
+  ::-webkit-scrollbar {
+    width: 12px; /* width of the entire scrollbar */
+  }
+
+  ::-webkit-scrollbar-track {
+    background: white; /* color of the tracking area */
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #d8d9dc; /* color of the scroll thumb */
+    border-radius: 20px; /* roundness of the scroll thumb */
+    //border: 3px solid orange; /* creates padding around scroll thumb */
+  }
 `;
