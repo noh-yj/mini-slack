4b10822e5ac8277febadd9cd9c18c0aff438b11a
noh-yj <goky456@gmail.com>
2021-04-20 19:59:07 +0900

    쿠키 삭제 path 추가

--- client/src/shared/Cookie.js
+++ client/src/shared/Cookie.js
@@ -3,14 +3,14 @@
 // 쿠키 저장
 const setCookie = (name, value) => {
   let date = new Date();
-  // 만료시간 5시간
+  // 만료 시간 5시간
   date.setTime(date.getTime() + 1 * 1000 * 60 * 60 * 5);
   document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
 };
 
 // 쿠키 삭제
 const deleteCookie = (name) => {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;';
+  document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT;path=/;';
 };
 
 // 쿠키 조회
